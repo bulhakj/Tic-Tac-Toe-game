709c150bb50516dcb7ebe5fb20a6cad0dd7db4b4
bulhakj <jakub.bulhak@gmail.com>
2018-11-14 15:19:12 +0100

    Blocking gameplay after someone is winner

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var activePlayer = 0;
-var markers = ["X", "O"]
-
+var markers = ["X", "O"];
+var c1, c2, c3;
 
 var board = document.querySelectorAll(".square");
 var informations = document.querySelector(".informations");
@@ -9,27 +9,41 @@ var logBoardIndex = function(boardIndex) {
     console.log('boardIndex:', boardIndex);
   }
 
+boardInit();
+
 function play() {
-    board.forEach(function test (square, index) {
-            board[index].addEventListener("click", function playing() {
-                logBoardIndex(index);
-                if(activePlayer == 0 || activePlayer == 1) {
-                    if(board[index].value == undefined && board[index].value != markers[activePlayer]) {
-                    board[index].value = markers[activePlayer];
-                    board[index].textContent = markers[activePlayer];
-                    console.log(getWinner());
-                    changePlayer();
+        board.forEach(function test (square, index) {
+                board[index].addEventListener("click", function playing() {
+                    logBoardIndex(index);
+                    if(activePlayer == 0 || activePlayer == 1) {
+                        // if(board[index].value == 0 && board[index].value != markers[activePlayer]) {
 
+                        if(board[index].value != markers[activePlayer]) {
+                        board[index].value = markers[activePlayer];
+                        board[index].textContent = markers[activePlayer];
+                        getWinner();
+                        changePlayer();
+                        }
                     }
-                }
-
+            })
         })
-    })
+
 }
+
 play();
 
-function checkingBoard() {
 
+function boardInit() {
+    for(i = 0; i < board.length; i++) {
+        board[i].value = 0;
+        board[i].textContent = "";
+        console.log("tablica" + board[i].value);
+    }
+}
+
+function blockBoard(board) {
+    board[5].removeEventListener("click", playing, false);
+    console.log("usuwam onclicka na 5 elemencie ");
 }
 
 function changePlayer() {
@@ -41,11 +55,16 @@ function changePlayer() {
 
     informations.textContent = "IT'S PLAYER'S " + markers[activePlayer] +" TURN.";
 }
+function blockBoard() {
+    document.querySelector(".board").style.pointerEvents = "none";
+}
 
 function selectWinnerCells(c1, c2, c3) {
     c1.classList.add("win");
     c2.classList.add("win");
     c3.classList.add("win");
+    blockBoard();
+
 }
 function getWinner() {
 
@@ -55,31 +74,31 @@ function getWinner() {
         console.log("win");
         selectWinnerCells(board[0], board[1], board[2]);
     }
-    if(board[3].innerHTML !== "" && board[3].innerHTML === board[4].innerHTML && board[3].innerHTML === board[5].innerHTML) {
+    else if(board[3].innerHTML !== "" && board[3].innerHTML === board[4].innerHTML && board[3].innerHTML === board[5].innerHTML) {
         console.log("win");
         selectWinnerCells(board[3], board[4], board[5]);
     }
-    if(board[6].innerHTML !== "" && board[6].innerHTML === board[7].innerHTML && board[6].innerHTML === board[8].innerHTML) {
+    else if(board[6].innerHTML !== "" && board[6].innerHTML === board[7].innerHTML && board[6].innerHTML === board[8].innerHTML) {
         console.log("win");
         selectWinnerCells(board[6], board[7], board[8]);
     }
-    if(board[0].innerHTML !== "" && board[0].innerHTML === board[3].innerHTML && board[0].innerHTML === board[6].innerHTML) {
+    else if(board[0].innerHTML !== "" && board[0].innerHTML === board[3].innerHTML && board[0].innerHTML === board[6].innerHTML) {
         console.log("win");
         selectWinnerCells(board[0], board[3], board[6]);
     }
-    if(board[1].innerHTML !== "" && board[1].innerHTML === board[4].innerHTML && board[1].innerHTML === board[7].innerHTML) {
+    else if(board[1].innerHTML !== "" && board[1].innerHTML === board[4].innerHTML && board[1].innerHTML === board[7].innerHTML) {
         console.log("win");
         selectWinnerCells(board[1], board[4], board[7]);
     }
-    if(board[2].innerHTML !== "" && board[2].innerHTML === board[5].innerHTML && board[2].innerHTML === board[8].innerHTML) {
+    else if(board[2].innerHTML !== "" && board[2].innerHTML === board[5].innerHTML && board[2].innerHTML === board[8].innerHTML) {
         console.log("win");
         selectWinnerCells(board[2], board[5], board[8]);
     }
-    if(board[0].innerHTML !== "" && board[0].innerHTML === board[4].innerHTML && board[0].innerHTML === board[8].innerHTML) {
+    else if(board[0].innerHTML !== "" && board[0].innerHTML === board[4].innerHTML && board[0].innerHTML === board[8].innerHTML) {
         console.log("win");
         selectWinnerCells(board[0], board[4], board[8]);
     }
-    if(board[2].innerHTML !== "" && board[2].innerHTML === board[4].innerHTML && board[2].innerHTML === board[6].innerHTML) {
+    else if(board[2].innerHTML !== "" && board[2].innerHTML === board[4].innerHTML && board[2].innerHTML === board[6].innerHTML) {
         console.log("win");
         selectWinnerCells(board[2], board[4], board[6]);
     }
@@ -88,4 +107,4 @@ function getWinner() {
 
 
 console.log(board);
-getWinner();
+// getWinner();
