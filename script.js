2efa8f205135e39d9bb2da3913c930ba717d139d
bulhakj <jakub.bulhak@gmail.com>
2018-11-19 14:00:48 +0100

    clearing whole board when someone wins includeing win classes.

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var activePlayer;
 var markers = ["X", "O"];
 var c1, c2, c3;
+var button;
+var winnerCell1, winnerCell2, winnerCell3;
 
 activePlayer = 0;
 
 var wholeBoard = document.querySelector(".board");
 var board = document.querySelectorAll(".square");
 var informations = document.querySelector(".informations");
+
+var player0 = document.getElementById("player-0");
+var player1 = document.getElementById("player-1");
 
 var logBoardIndex = function (boardIndex) {
     console.log('boardIndex:', boardIndex);
@@ -41,6 +46,7 @@ function boardInit() {
         board[i].textContent = "";
         console.log("tablica" + board[i].value);
     }
+
     return true;
 }
 
@@ -48,37 +54,75 @@ function changePlayer() {
     // Changing player
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 
-    document.getElementById("player-0").classList.toggle("active-player");
-    document.getElementById("player-1").classList.toggle("active-player");
+    player0.classList.toggle("active-player");
+    player1.classList.toggle("active-player");
+
+    viewActivePlayerText();
+
+    return player0, player1;
+}
+
+function viewActivePlayerText() {
 
     informations.textContent = "PLAYER " + markers[activePlayer] + " TURN.";
+
 }
 
 function createNewGameBtn() {
-    // var player = document.querySelector("#player-" + activePlayer).classList.add("winner-theme");
-    // player.innerHTML = "WINNER!";
-    // var button = document.querySelector(".informations").classList.add("new-game-button");
-    var button = document.querySelector(".informations");
+
     var winner = document.getElementById("player-" + activePlayer);
-    button.classList.add("new-game-button");
+    informations.classList.add("new-game-button");
     console.log("active przed evalem" + activePlayer);
     winner.textContent = "WINNER!";
-    eval(button.innerHTML = "NEW GAME");
     console.log(activePlayer);
-    // winner.textContent = "WINNER!";
+    listenerNewGameBtn();
+    console.log("EVAL");
+    eval(informations.innerHTML = "NEW GAME");
 
 }
+
+
+function listenerNewGameBtn() {
+
+    informations.onclick = clearNames;
+
+}
+
+
+function clearNames() {
+
+    player0.classList.remove("active-player");
+    player1.classList.remove("active-player");
+    player0.textContent = "PLAYER X";
+    player1.textContent = "PlAYER O";
+    informations.classList.remove("new-game-button");
+    activePlayer = 0;
+    player0.classList.add("active-player");
+    boardInit();
+    deleteWinClass();
+    play();
+}
+
+function deleteWinClass() {
+
+    for (i = 0; i < board.length; i++) {
+
+        board[i].classList.remove("win");
+    }
+}
+
 function blockBoard() {
     wholeBoard.style.pointerEvents = "none";
+
 }
 
 function selectWinnerCells(c1, c2, c3) {
     c1.classList.add("win");
     c2.classList.add("win");
     c3.classList.add("win");
+    console.log("winnercell1 = " + winnerCell1);
     blockBoard();
     createNewGameBtn();
-    // eval(informations.innerHTML = "NEW GAME");
 
 }
 function getWinner() {
