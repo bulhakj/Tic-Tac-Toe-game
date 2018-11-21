7e188c09ed26ec112b854b08838fc3264ef981ee
bulhakj <jakub.bulhak@gmail.com>
2018-11-21 20:23:03 +0100

    added draw funcionality and new game button after draw.

--- script.js
+++ script.js
@@ -2,22 +2,29 @@ var activePlayer;
 var markers = ["X", "O"];
 var c1, c2, c3;
 var button;
-var winnerCell1, winnerCell2, winnerCell3;
+var isEmpty = 0;
+var counter = 0;
 
-activePlayer = 0;
 
 var wholeBoard = document.querySelector(".board");
 var board = document.querySelectorAll(".square");
 var informations = document.querySelector(".informations");
-
 var player0 = document.getElementById("player-0");
 var player1 = document.getElementById("player-1");
+
 
 var logBoardIndex = function (boardIndex) {
     console.log('boardIndex:', boardIndex);
 }
 
+
+activePlayer = 0;
+var counter = 0;
+
 boardInit();
+play();
+
+
 
 function play() {
     board.forEach(function test(square, index) {
@@ -25,19 +32,28 @@ function play() {
             logBoardIndex(index);
             // if (activePlayer == 0 || activePlayer == 1) {
                 if(board[index].value == 0 && board[index].value != markers[activePlayer]) {
-
-                if (board[index].value != markers[activePlayer]) {
                     board[index].value = markers[activePlayer];
                     board[index].textContent = markers[activePlayer];
+                    counter++;
+                    console.log("długość countera: " + counter);
+                    // checkDraw();
                     getWinner();
+                    // check();
                     changePlayer();
-                }
+
+
             }
         })
     })
+    wholeBoard.style.pointerEvents = "all";
+    for (i = 0; i < board.length; i++) {
+        board[i].value = 0;
+        board[i].textContent = "";
+        console.log("tablica" + board[i].value);
+    }
+
 }
 
-play();
 
 function boardInit() {
     wholeBoard.style.pointerEvents = "all";
@@ -46,7 +62,6 @@ function boardInit() {
         board[i].textContent = "";
         console.log("tablica" + board[i].value);
     }
-
     return true;
 }
 
@@ -59,6 +74,7 @@ function changePlayer() {
 
     viewActivePlayerText();
 
+
     return player0, player1;
 }
 
@@ -70,17 +86,35 @@ function viewActivePlayerText() {
 
 function createNewGameBtn() {
 
-    var winner = document.getElementById("player-" + activePlayer);
     informations.classList.add("new-game-button");
-    console.log("active przed evalem" + activePlayer);
-    winner.textContent = "WINNER!";
     console.log(activePlayer);
     listenerNewGameBtn();
-    console.log("EVAL");
     eval(informations.innerHTML = "NEW GAME");
 
 }
+function settingWinner() {
+    var winner = document.getElementById("player-" + activePlayer);
+    winner.textContent = "WINNER!";
+    console.log("setting winner");
+}
 
+function draw() {
+    settingDraw();
+    createNewGameBtn();
+    listenerNewGameBtn();
+
+}
+
+function settingDraw() {
+
+    player0.textContent = "DRAW";
+    player1.textContent = "DRAW";
+    if(activePlayer === 0) {
+        player1.classList.add("active-player");
+    } else {
+        player0.classList.add("active-player");
+    }
+}
 
 function listenerNewGameBtn() {
 
@@ -96,11 +130,17 @@ function clearNames() {
     player0.textContent = "PLAYER X";
     player1.textContent = "PlAYER O";
     informations.classList.remove("new-game-button");
-    activePlayer = 0;
-    player0.classList.add("active-player");
+    counter = 0;
+    setActivePlayer();
     boardInit();
     deleteWinClass();
+    viewActivePlayerText();
     play();
+}
+
+function setActivePlayer() {
+    activePlayer = 0;
+    player0.classList.add("active-player");
 }
 
 function deleteWinClass() {
@@ -108,21 +148,26 @@ function deleteWinClass() {
     for (i = 0; i < board.length; i++) {
 
         board[i].classList.remove("win");
+
     }
 }
 
 function blockBoard() {
     wholeBoard.style.pointerEvents = "none";
+    console.log("funkcja block board");
 
 }
+
+
 
 function selectWinnerCells(c1, c2, c3) {
     c1.classList.add("win");
     c2.classList.add("win");
     c3.classList.add("win");
-    console.log("winnercell1 = " + winnerCell1);
+    settingWinner();
     blockBoard();
     createNewGameBtn();
+
 
 }
 function getWinner() {
@@ -132,6 +177,7 @@ function getWinner() {
     if (board[0].innerHTML !== "" && board[0].innerHTML === board[1].innerHTML && board[0].innerHTML === board[2].innerHTML) {
         console.log("win");
         selectWinnerCells(board[0], board[1], board[2]);
+
     }
     else if (board[3].innerHTML !== "" && board[3].innerHTML === board[4].innerHTML && board[3].innerHTML === board[5].innerHTML) {
         console.log("win");
@@ -161,9 +207,15 @@ function getWinner() {
         console.log("win");
         selectWinnerCells(board[2], board[4], board[6]);
     }
+    else {
+        if(counter === 9) {
+            console.log("remis");
+            draw();
+        }
+    }
+
 
 }
-
 
 console.log(board);
 // getWinner();
